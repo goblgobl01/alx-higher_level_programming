358f18729ca948c7b115cf4ae07af8e3c660c8ef
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-12-05 00:14:31 +0100

    alo

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
+    length = len(my_list)
 
-	length = len(my_list)
-
-	if(idx > (length-1)):
-		return("None")
-	elif(idx < 0):
-		return("None")
-	else:
-		return(my_list[idx])
+    if idx > (length - 1) or idx < 0:
+        return None
+    else:
+        return my_list[idx]
