b8a744baba3bb49a7fdf52eda4bcdf647819b034
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-12-05 01:09:08 +0100

    alo

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
+
+def print_reversed_list_integer(my_list=None):
+    if my_list is None:
+        my_list = []
+
     length = len(my_list)
     
     for i in range(-1, -(length+1), -1):
