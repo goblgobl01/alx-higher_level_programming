11f32bac9a8c12a6441d1679e716cc575a59138b
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-12-05 01:48:34 +0100

    alo

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python3
+
 def divisible_by_2(my_list=None):
-    bool_list = my_list[:] if my_list else []
-    for count, num in enumerate(my_list):
-        bool_list[count] = True if num % 2 == 0 else False
+    if my_list is None:
+        my_list = []
+        
+    bool_list = my_list[:]
+    for count, i in enumerate(my_list):
+        bool_list[count] = i % 2 == 0
     return bool_list
 
 def delete_at(my_list=None, idx=0):
+    if my_list is None:
+        my_list = []
+
     if 0 <= idx < len(my_list):
         del my_list[idx]
+        
     return my_list
