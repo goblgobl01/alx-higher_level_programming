5b6f758948fa3f7f67e566621d9cfe53dcfe97b2
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-12-06 10:09:44 +0100

    alo

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,6 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    rows = len(matrix)
-    cols = len(matrix[0])
-    new_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
-
-    for i in range(rows):
-        for j in range(cols):
-            element = matrix[i][j]
-            new_matrix[i][j] = element ** 2
-    return new_matrix
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
