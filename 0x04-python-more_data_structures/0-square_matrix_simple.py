4896794e9e0c8d7f995064f9c8d8edba1dd45876
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-12-06 10:06:45 +0100

    alo

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 def square_matrix_simple(matrix=[]):
     rows = len(matrix)
-    cols = len(matrix[0]) if rows > 0 else 0  # Assuming all rows have the same length
-
+    cols = len(matrix[0]) if rows > 0 else 0
     new_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
 
     for i in range(rows):
