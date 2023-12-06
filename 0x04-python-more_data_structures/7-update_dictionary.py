1a21447ee08e0cc013cc7e732163d309ad4550ca
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-12-06 11:27:41 +0100

    alo

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key : value})
+    """
+    Update a dictionary with the provided key-value pair.
+
+    Args:
+    - a_dictionary (dict): The dictionary to be updated.
+    - key: The key to be added or updated.
+    - value: The value corresponding to the key.
+
+    Returns:
+    - dict: The updated dictionary.
+    """
+    a_dictionary.update({key: value})
     return a_dictionary
