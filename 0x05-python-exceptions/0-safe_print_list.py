d46d47ce6bc264a9e2a25fa9501ef06778c0bb71
Mohammed Maarafi <maarafimohammed@gmail.com>
2023-12-18 19:44:26 +0100

    alo

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(x):
+    for i in my_list:
         try:
-            print(my_list[i], end="")
-            count += 1
+            if (count >= x):
+                raise Exception("Reached given limit.")
+            print(f"{i}", end="")
+        except Exception:
+            print("")
+            return count
         except IndexError:
-            break
-    print("\n", end="")
+            print("")
+            return count
+        count += 1
+    print("")
     return count
