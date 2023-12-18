bdd04ee0015cb56d228893f64cc0cf5110eaeb1c
Mohammed Maarafi <maarafimohammed@gmail.com>
2023-12-18 14:53:09 +0100

    alo

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,15 @@
-def square_matrix_simple(matrix=[]):
-    squared = []
-    for line in matrix:
-        squared.append([c**2 for c in line])
-    return squared
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    for i in my_list:
+        if count == x:
+            break
+        else:
+            try:
+                print("{:d}".format(i))
+                count += 1
+            except:
+                print("unknown error")
+
