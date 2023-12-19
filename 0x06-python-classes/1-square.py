57830b3c457b8ea5f2c8dce55f89dc84447f06e9
Mohammed Maarafi <maarafimohammed@gmail.com>
2023-12-19 20:03:12 +0100

    alo

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """this is the square class documentation """
+    def __init__(self, size):
+        """this si the constructor part of the class we
+        Args:
+            __size: a private attribute of a instance
+        """
+        self.__size = size
