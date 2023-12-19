35b78fdfa0dfba36c4b24afa102295db98063934
Mohammed Maarafi <maarafimohammed@gmail.com>
2023-12-19 20:20:09 +0100

    alo

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """this is the square class documentation """
+    def __init__(self, size=0):
+        """this si the constructor part of the class we
+        Args:
+            __size: a private attribute of a instance
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """this si the constructor part of the class we
+        Return:
+            __size: a private attribute of a instance
+        """
+        return self.__size*self.__size
