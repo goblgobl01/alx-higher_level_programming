fea6405a3f704a4d4df5f7008d032ae0184fe92b
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-01-29 14:48:10 +0100

    alo

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,6 +43,8 @@ class Rectangle:
         self.__height = value
     def area(self):
         """return the are of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return self.__width * self.__height
 
     def perimeter(self):
