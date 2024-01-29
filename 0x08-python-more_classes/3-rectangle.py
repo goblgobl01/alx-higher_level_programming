46a86eededae1cf92d2866ac1c419169d3e3aa20
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-01-29 15:33:48 +0100

    alo

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,7 +52,10 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """retun a representation of the triangle"""
+        """Return the printable representation of the Rectangle.
+
+        Represents the rectangle with the # character.
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         return ((self.__width * "#") + "\n") * self.__height
