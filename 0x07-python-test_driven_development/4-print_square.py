e6945700afba3084f09ecb7ec6a40588b9a1ac2e
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-02-02 14:04:55 +0100

    alo

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,5 +7,5 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if not isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-
-    print(((size * "#") + "\n") * size)
+    string = ((size * "#") + "\n") * size
+    print(string[:-1])
