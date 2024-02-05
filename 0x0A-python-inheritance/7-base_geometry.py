079a4090db2356323747e39119d6189d8932f347
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-02-05 22:02:45 +0100

    alo

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""a empty class."""
+"""Module for 7-base_geometry.py"""
 
 
 class BaseGeometry:
-    """a empty class"""
+    """an empty class BaseGeometry"""
     def area(self):
-        """raise a exeption."""
+        """Public instance method that raises an exception"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
-        """valide Value."""
-        if not isinstance(value, int):
+        """Public instance method that validates value"""
+        if type(value) != int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
