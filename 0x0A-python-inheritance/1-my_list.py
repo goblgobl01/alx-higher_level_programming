eb8ae1207e2ed58a2421cf830a849853872c5ed0
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-02-05 20:50:18 +0100

    alo

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
+"""a class that inherit from the list class."""
+
 
 class MyList(list):
-    """
-    MyList is a custom subclass of the built-in list class in Python.
-
-    Attributes:
-        Inherits all attributes from the list class.
-
-    Methods:
-        print_sorted():
-            Prints the elements of the list in sorted order.
-
-    Example Usage:
-        my_list = MyList([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])
-        my_list.print_sorted()
-        # Output: [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
-    """
+    """the list class sub class."""
     def print_sorted(self):
-        """
-        Print the elements of the list in sorted order.
-
-        Returns:
-            None
-        """
+        """print a list in ascending order."""
         print(sorted(self))
