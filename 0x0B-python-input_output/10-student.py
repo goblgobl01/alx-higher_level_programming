ee131c5ae806de280009d55da36acb35e3ed3844
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-02-08 00:24:44 +0100

    alo

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,6 +4,7 @@
 
 class Student:
     """A class that defines a student."""
+
     def __init__(self, first_name, last_name, age):
         """The class constructor."""
         self.first_name = first_name
@@ -17,11 +18,11 @@ class Student:
         """
         if attrs is None:
             return {'first_name': self.first_name,
-                'last_name': self.last_name, 'age': self.age}
+                    'last_name': self.last_name, 'age': self.age}
         else:
             dict = {}
             for attributes in attrs:
-                if not attributes in self.__dict__:
+                if attributes not in self.__dict__:
                     continue
                 dict[attributes] = getattr(self, attributes)
             return dict
