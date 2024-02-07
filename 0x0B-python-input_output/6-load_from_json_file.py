4238c99f90782d780c626777e98c6d9ae466cc1b
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-02-07 16:19:58 +0100

    alo

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""6-load_from_json_file.py python module."""
+
+
+def load_from_json_file(filename):
+    """a function that turn text file data to its orginal form."""
+    with open(filename, "r", encoding="utf-8") as file:
+        import json
+        json.load(file)
