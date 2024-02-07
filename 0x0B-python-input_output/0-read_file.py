61910dfb2c919920c56238e8063e8afc7c47b4f6
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-02-07 15:19:21 +0100

    alo

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """a function that read a file and print it to stdout."""
-    with open(filename, encoding="utf-8") as file:
-        data = file.read()
-        print(data)
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
