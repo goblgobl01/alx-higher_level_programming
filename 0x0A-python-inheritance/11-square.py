96f01ad374455f57cfc1032b7e2f6dfda8d61385
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-02-08 01:46:26 +0100

    alo

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""10-square python module."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """the grand child class."""
+
+    def __init__(self, size):
+        """construcor of the square class."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """return the area of the square."""
+        return self.__size**2
+
+    def __str__(self):
+        """return a simple string"""
+        return f"[Square] {self.__size}/{self.__size}"
