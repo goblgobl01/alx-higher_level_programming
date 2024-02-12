a151f700f457041da71da086414125ec0cea3827
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-02-12 23:25:33 +0100

    alo

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""base python module."""
+
+
+class Base:
+    """base class i dont know what its for but ok"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+"""rectangle python module."""
+from .base import Base
+
+class Rectangle(Base):
+    """a class that define a rectangle."""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """class constructor."""
+        super().__init__(id)
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+
+    @property
+    def x(self):
+        """Get the x private attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """set the x private attributes"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """get the private attributes y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """set the private attributes y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def display(self):
+        """print representation of the rectangle to the stdout."""
+        list = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                list = list + "#"
+            list = list + "\n"
+        print(list[:-1])
+
+    def __str__(self):
+        """return a simple string."""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
