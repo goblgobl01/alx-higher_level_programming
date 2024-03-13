37b7185ef5fabaebd07af0df02f5c90504c0230c
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-03-13 16:07:16 +0000

    alo

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- this script is gonna show the priviligeas a user have.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+this is the readme file for the 2nd project of mysql
