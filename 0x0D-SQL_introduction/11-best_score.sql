485edf0b590c05945612f1e2b2fe74c50b168b96
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-03-13 12:58:53 +0000

    alo

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- select parameters from a table and oreder them
-SELECT score, name FROM second_table WHERE score >= 10;
+SELECT score, name FROM `second_table` WHERE score >= 10 ORDER BY score DESC;
