f6e5d205332b8d8bd4aedc7241042f4bda3d23ff
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-04-11 11:36:53 +0000

    possible fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,6 +3,4 @@ if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log(process.argv[2]);
-} else {
-  console.log(process.argv[2]);
 }
