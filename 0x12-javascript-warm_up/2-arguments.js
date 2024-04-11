af5559eb1e07dcd53af460e933d638d74cfe2c0a
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-04-11 11:28:32 +0000

    another one

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (args.length === 3) {
-  console.log('argument found');
-}	else {
-  console.log('argument found');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
