59d3e3323a72c27133f466b06d9f6191cbc8e8c7
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-04-14 11:50:40 +0100

    all the values

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          db=sys.argv[3])
     cur = db.cursor()
     query = f"""SELECT * FROM states WHERE name
-            LIKE BINARY '{sys.argv[4]}' ORDER BY id ASC"""
+            LIKE BINARY '%{sys.argv[4]}%' ORDER BY id ASC"""
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
