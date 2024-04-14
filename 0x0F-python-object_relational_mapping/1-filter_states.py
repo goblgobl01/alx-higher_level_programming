49a4e2d3102254fc04e272b252e33b0407798238
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-04-14 11:30:06 +0100

    second task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                                  passwd=sys.argv[2],
                                  db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE ")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
