65e61cc93ae14f11af785cd381b53c8b1a001e7f
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-04-14 11:24:39 +0100

    alo

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                                  passwd=sys.argv[2],
                                  db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE ")
     rows = cur.fetchall()
     for row in rows:
         print(row)
